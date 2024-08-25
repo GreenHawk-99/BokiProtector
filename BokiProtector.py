4d575549bc48b32860e018ce11ad80366955d539
GreenHawk <82842840+GreenHawk-99@users.noreply.github.com>
2024-08-25 22:13:35 +0200

    [1.1.0][RLS]
    * [FTR] Custom salt
    
    * README updated + new exe

--- BokiProtector.py
+++ BokiProtector.py
@@ -2,12 +2,23 @@ import hashlib
 import secrets  # More secure than random for cryptographic purposes
 
 
-def hash_password(password: str) -> str:
-    # Generate a more secure salt
-    salt = f"Boki-Protector-{secrets.randbelow(1999 - 122) + 122}-"
+def hash_password(given_password: str, given_salt: str) -> str:
+
+    if given_salt:
+        try:
+            # Generate a less secure salt
+            salt = f"Boki-Protector-{int(given_salt)}-"
+        except ValueError:
+            # Generate a more secure salt
+            print("[ERROR] Invalid salt. Using a random secure salt instead.")
+            salt = f"Boki-Protector-{secrets.randbelow(1999 - 122) + 122}-"
+    else:
+        print("[INFO] Custom salt skipped")
+        # Generate a more secure salt
+        salt = f"Boki-Protector-{secrets.randbelow(1999 - 122) + 122}-"
 
     # Combine salt and password
-    salted_password = salt + password
+    salted_password = salt + given_password
 
     # Hash the salted password
     hashed = hashlib.sha3_512(salted_password.encode()).hexdigest()
@@ -18,13 +29,15 @@ def hash_password(password: str) -> str:
 if __name__ == "__main__":
     # Prompt the user for their password
     password = input("[INFO] Enter your password: ")
+    custom_salt = input("[INFO] (Optional) Enter your custom salt (must be a number) "
+                        "or press 'Enter' to skip: ")
 
     # Hash the password
-    hashed_password = hash_password(password)
+    hashed_password = hash_password(password, custom_salt)
 
     # Show the result
     print("[SUCCESS] Here is the hashed password:")
     print(hashed_password)
 
     # Pause the console before closing
-    input("[INFO] Press Enter to close the program")
+    input("[INFO] Press 'Enter' to close the program")
